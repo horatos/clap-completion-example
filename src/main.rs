dcfc2650397a2b76d410cc2d6fe2f7a67f6e5373
Shotaro Tsuji <Shotaro.Tsuji@gmail.com>
2022-01-09 19:37:17 +0900

    Add `struct Cli` which derives `clap::Parser`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::path::PathBuf;
+
+use clap::{ArgEnum, Parser};
+
+#[derive(Parser,Debug)]
+struct Cli {
+    #[clap(long,short,arg_enum)]
+    language: Option<Language>,
+    #[clap(long,short)]
+    file: Option<PathBuf>,
+}
+
+#[derive(ArgEnum,Clone,Debug)]
+enum Language {
+    En,
+    Ja,
+}
+
 fn main() {
+    let _cli = Cli::parse();
+
     println!("Hello");
 }
